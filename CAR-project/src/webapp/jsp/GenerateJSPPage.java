08c31095c3827e669ee7d7d2ca15eb4f58b4d125
jingshu <f.shujing@msn.com>
2013-11-15 21:40:23 +0100

    the problem of generation of jsp (how to protect <%@ and %> as literal charactors) is resolved

--- CAR-project/src/webapp/jsp/GenerateJSPPage.java
+++ CAR-project/src/webapp/jsp/GenerateJSPPage.java
@@ -15,7 +15,12 @@ public class GenerateJSPPage
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "<\\%@ page language=\\\"java\\\" contentType=\\\"text/html; charset=US-ASCII\\\" pageEncoding=\\\"US-ASCII\\\"\" %>" + NL + "<\\%@ taglib prefix=\"html\" uri=\"http://struts.apache.org/tags-html\" %\\>" + NL + "<\\%@ taglib prefix=\"bean\" uri=\"http://struts.apache.org/tags-bean\" %\\>" + NL + "<\\%@ taglib prefix=\"logic\" uri=\"http://struts.apache.org/tags-logic\" %\\> " + NL + "<\\%@ taglib prefix=\"nested\" uri=\"http://struts.apache.org/tags-nested\" %\\>" + NL + "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">";
+  protected final String TEXT_1 = "";
+  protected final String TEXT_2 = NL;
+  protected final String TEXT_3 = NL;
+  protected final String TEXT_4 = NL;
+  protected final String TEXT_5 = NL;
+  protected final String TEXT_6 = NL + "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">";
 
   public String generate(Object argument)
   {
@@ -23,8 +28,17 @@ public class GenerateJSPPage
       Page page = (Page)argument;
 	StringBuffer content = new StringBuffer();
 	
-	
     stringBuffer.append(TEXT_1);
+    stringBuffer.append( "<%@ page language=\"java\" contentType=\"text/html; charset=US-ASCII\" pageEncoding=\"US-ASCII\" %>" );
+    stringBuffer.append(TEXT_2);
+    stringBuffer.append( "<%@ taglib prefix=\"html\" uri=\"http://struts.apache.org/tags-html\" %>" );
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append( "<%@ taglib prefix=\"bean\" uri=\"http://struts.apache.org/tags-bean\" %>" );
+    stringBuffer.append(TEXT_4);
+    stringBuffer.append( "<%@ taglib prefix=\"logic\" uri=\"http://struts.apache.org/tags-logic\" %> " );
+    stringBuffer.append(TEXT_5);
+    stringBuffer.append( "<%@ taglib prefix=\"nested\" uri=\"http://struts.apache.org/tags-nested\" %>" );
+    stringBuffer.append(TEXT_6);
     return stringBuffer.toString();
   }
 }
